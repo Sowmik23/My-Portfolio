2e85ea7c9a78a202f3891e43e8f5ff96145105eb
Sowmik23 <asowmik16@gmail.com>
2019-08-16 00:23:25 +0600

    Updates

---
+++ precache-manifest.5fc5f94ff5c867dc070518c0e66e190f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b28033a9f9f4e0db0861815d8aa24a9",
+    "url": "/My-Portfolio/index.html"
+  },
+  {
+    "revision": "f913d90e4826c4321b2d",
+    "url": "/My-Portfolio/static/css/2.60a6b8e0.chunk.css"
+  },
+  {
+    "revision": "4f5c790a259e273dcd4a",
+    "url": "/My-Portfolio/static/css/main.04ad4a19.chunk.css"
+  },
+  {
+    "revision": "f913d90e4826c4321b2d",
+    "url": "/My-Portfolio/static/js/2.bd880b47.chunk.js"
+  },
+  {
+    "revision": "4f5c790a259e273dcd4a",
+    "url": "/My-Portfolio/static/js/main.cef965a9.chunk.js"
+  },
+  {
+    "revision": "b7d4bf292e75006047a4",
+    "url": "/My-Portfolio/static/js/runtime~main.f7066801.js"
+  }
+]);
